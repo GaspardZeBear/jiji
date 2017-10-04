c996552c5829d3a46760493ceae6b9b6015af183
SYLVAIN VUILLEMET <sylvain.vuillemet@worldline.com>
2017-10-04 08:09:55 +0000

    Refactoring

--- Utils/Jira.py
+++ Utils/Jira.py
@@ -7,17 +7,25 @@ from Config import *
 #----------------------------------------------------------------
 class Jira :
 
+  def jiraPost(self,request,url) :
+    return(Config.CURLPOST + ' --data ' + request + ' ' +  Config.JIRAURL + url)
+
   def getSample(self):
     fields='["id","key","summary","components","creator","created","updated","status","customfield_30941","customfield_10370","customfield_11730"]'
     request='\'{"jql":"project =' + Config.JIRAPROJECT +'","startAt":0,"maxResults":1}\''
     cmd=Config.CURL + ' -X POST -H "Content-Type: application/json" --data ' + request + ' ' + Config.JIRAURL+'/rest/api/2/search'
-    return(self.invoke(cmd))
+    cmd=Config.CURL + ' ' + 'https://jira.itsm.atosworldline.com/rest/api/latest/issue/SDCOBENCH-243/'
 
+    return(self.invoke(cmd))
 
   def getJiraList(self):
     fields='["id","key","summary","components","creator","created","updated","status","customfield_30941","customfield_10370","customfield_11730","assignee","reporter"]'
     request='\'{"jql":"project =' + Config.JIRAPROJECT +'","startAt":0,"maxResults":1000,"fields":' + fields + '}\''
-    cmd=Config.CURL + ' -X POST -H "Content-Type: application/json" --data ' + request + ' ' +  Config.JIRAURL+'/rest/api/2/search'
+    cmd=self.jiraPost(request,'/rest/api/2/search')
+    return(self.invoke(cmd))
+
+  def getIssue(self,num):
+    cmd=Config.CURL + ' ' +  Config.JIRAURL +'/rest/api/latest/issue/' + Config.JIRAPROJECT + '-' + str(num) 
     return(self.invoke(cmd))
 
   def getComments(self,num):
@@ -25,9 +33,10 @@ class Jira :
     return(self.invoke(cmd))
 
   def addComment(self,num,comment):
-    print(comment)
     request='\'{"body": "' + comment[0] + '"}\''
-    cmd=Config.CURL + ' -X POST -H "Content-Type: application/json" --data ' + request + ' ' +  Config.JIRAURL+'/rest/api/latest/issue/' + Config.JIRAPROJECT + '-' + str(num) + '/comment'
+    #cmd=Config.CURL + ' -X POST -H "Content-Type: application/json" --data ' + request + ' ' +  Config.JIRAURL+'/rest/api/latest/issue/' + Config.JIRAPROJECT + '-' + str(num) + '/comment'
+    url='/rest/api/latest/issue/' + Config.JIRAPROJECT + '-' + str(num) + '/comment'
+    cmd=self.jiraPost(request,url)
     return(self.invoke(cmd))
 
   def getTransitions(self,num):
--- jiji.py
+++ jiji.py
@@ -25,6 +25,24 @@ def fSample(args=None) :
 
 
 #----------------------------------------------------------------
+def displayIssue(datas) :
+  f=datas["fields"]
+  print('{:13.13};{};{:5.5};{};{};{:3.3};{:3.3};{:3.3};{:50.50};{:40.40};{:30.30}'.format(
+      datas["key"],
+      status[f["status"]["name"]],
+      f["components"][0]["name"],
+      f["created"][0:10],
+      f["updated"][0:10],
+      f["creator"]["emailAddress"][0:20],
+      f["assignee"]["emailAddress"][0:20],
+      f["reporter"]["emailAddress"][0:20],
+      f["summary"].encode('ascii','replace'),
+      f["customfield_10370"]["value"],
+      f["customfield_11730"]
+  ))
+
+
+#----------------------------------------------------------------
 def fList(args=None) :
   logging.debug(pformat(args) )
   jira=Jira()
@@ -47,42 +65,22 @@ def fList(args=None) :
     if args.assignee and re.search(args.assignee[0],f["assignee"]["emailAddress"]) is None :
       #print("filtered by assignee")
       continue
-    print('{:13.13};{};{:5.5};{};{};{:3.3};{:3.3};{:3.3};{:50.50};{:40.40};{:30.30}'.format(
-      d["key"],
-      status[f["status"]["name"]],
-      f["components"][0]["name"],
-      f["created"][0:10],
-      f["updated"][0:10],
-      f["creator"]["emailAddress"][0:20],
-      f["assignee"]["emailAddress"][0:20],
-      f["reporter"]["emailAddress"][0:20],
-      f["summary"].encode('ascii','replace'),
-      f["customfield_10370"]["value"],
-      f["customfield_11730"]
-     ))
+    displayIssue(d)
   return
 #----------------------------------------------------------------
 def fCache(args=None) :
   return
 
 #----------------------------------------------------------------
-def fInspect(args=None) :
-  logging.debug(pformat(args) )
-  jira=Jira()
-  datas=jira.getComments(args.jirano)
-  #pprint(datas)
-  mark='-'*100
-  for d in datas["comments"] :
-    print("\n{}\n;Comment;{:10.10};Author;{:50.50}\n{}\n{}".format(
-     mark,
-     d["created"],
-     d["author"]["emailAddress"],
-     mark,
-     d["body"]
-    ))
-  datas=jira.getTransitions(args.jirano)
-  print("\n")
+def printMark(section) :
+  mark='='*100
+  print("\n"+mark)
+  print(section)
   print(mark)
+
+#----------------------------------------------------------------
+def showTransitions(datas) :
+  printMark('Transitions')
   for d in datas["transitions"] :
     print("Next allowed status;{:5.5};{:10.10};{:100.100}".format(
      d["id"],
@@ -90,7 +88,40 @@ def fInspect(args=None) :
      d["to"]["description"]
     ))
   #pprint(datas)
+
+#----------------------------------------------------------------
+def showComments(datas) :
+  printMark('Comments')
+  for d in datas["fields"]["comment"]["comments"] :
+    print("\n{}\n;Comment;{:10.10};Author;{:50.50}\n{}\n{}".format(
+     '-'*100,
+     d["created"],
+     d["author"]["emailAddress"],
+     '-'*100,
+     d["body"]
+    ))
   return
+
+#----------------------------------------------------------------
+def showHeader(datas) :
+  printMark('Headers')
+  f=datas["fields"]
+  displayIssue(datas)
+
+#----------------------------------------------------------------
+def fInspect(args=None) :
+  logging.debug(pformat(args) )
+  jira=Jira()
+  datas=jira.getIssue(args.jirano)
+  if 'H' in args.show[0] :
+    showHeader(datas)
+  if 'C' in args.show[0] :
+    showComments(datas)
+  if 'T' in args.show[0] :
+    datasT=jira.getTransitions(args.jirano)
+    showTransitions(datasT)
+  return
+
 
 #----------------------------------------------------------------
 def fComment(args=None) :
@@ -134,7 +165,7 @@ parserCache.add_argument('num',nargs='?',help="num of file to cache")
 parserInspect = subparsers.add_parser('inspect', help='a help')
 parserInspect.set_defaults(func=fInspect)
 parserInspect.add_argument('jirano',nargs='?',help="item to inspect (given by list)")
-parserInspect.add_argument('--show','-s',nargs=1,help="Comments, Transitions",default=['CT'])
+parserInspect.add_argument('--show','-s',nargs=1,help="Comments, Transitions",default=['CHT'])
 
 parserComment = subparsers.add_parser('comment', help='a help')
 parserComment.set_defaults(func=fComment)
